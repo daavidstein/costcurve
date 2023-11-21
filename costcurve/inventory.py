265d869e7d00d6d7880bd08eb05b29ac8f2f12ce
Daavid Stein <daavid@dendra.io>
2023-11-21 15:21:37 -0500

    parse_line_items, retry decorator

--- costcurve/inventory.py
+++ costcurve/inventory.py
@@ -2,17 +2,18 @@ import networkx as nx
 from collections import Counter,defaultdict
 from typing import List, Tuple, Set, Dict
 from nlp import isit
+
 class Inventory:
     def __init__(self, nodes: Set[str], edges: Set[Tuple[str, str]]):
 
-        self.validate_edges(edges)
+        self._validate_edges(edges)
         self.graph = nx.DiGraph()
         self.graph.add_nodes_from(nodes)
         self.graph.add_edges_from(edges)
-        self.set_indices()
-        self.code_map = self.create_codes()
+        self._set_indices()
+        self.code_map = self._create_codes()
 
-    def validate_edges(self, edges: Set[Tuple[str, str]]):
+    def _validate_edges(self, edges: Set[Tuple[str, str]]):
         """
         Check that each child has only one parent
 
@@ -23,7 +24,8 @@ class Inventory:
         if len(children) != len(set(children)):
             raise Exception("Some nodes have more than one parent!")
 
-    def set_indices(self):
+    def _set_indices(self):
+        """Initialize the indices of the nodes. The index is the digit in the place value that corresponds to the node depth"""
         seen = Counter()
         indices = dict()
         for e in self.graph.edges:
@@ -37,77 +39,97 @@ class Inventory:
 
         nx.set_node_attributes(self.graph, indices, "index")
 
-    def get_parent(self, node: str) -> str:
+    def _get_children(self, node: str) -> List[str]:
+        """Get the children of a node. Ie, get all x such that there is an edge (node,x) in the graph"""
+
+        if not node in self.graph.nodes:
+            raise Exception(f"{node} not in nodes: {list(self.graph.nodes)}")
+
+        children = [child for (parent, child) in self.graph.edges if parent == node]
+        return children
+
+
+    def _get_parent(self, node: str) -> str:
         if not node in self.graph.nodes:
             raise Exception(f"{node} not in nodes: {list(self.graph.nodes)}")
         for edge in self.graph.edges:
             if edge[1] == node:
                 return edge[0]
 
-    def get_ancestors(self, node, ancestors=None) -> List[str]:
+    def _get_ancestors(self, node, ancestors=None) -> List[str]:
         """Get the ancestors of a node recursively.
         we use the convention that every node is an ancestor of itself"""
 
         ancestors = ancestors or [node]
-        parent = self.get_parent(node)
+        parent = self._get_parent(node)
         if not parent:
             return ancestors
         else:
             ancestors.append(parent)
-            anc = self.get_ancestors(parent, ancestors)
+            anc = self._get_ancestors(parent, ancestors)
             return anc
 
-    def get_depth(self, node: str) -> int:
+    def _get_depth(self, node: str) -> int:
         """Get the number of ancestors of a node (including self)"""
-        return len(self.get_ancestors(node))
+        return len(self._get_ancestors(node))
 
-    def get_max_depth(self):
-        return max([self.get_depth(nd) for nd in self.graph.nodes])
+    def _get_max_depth(self):
+        return max([self._get_depth(nd) for nd in self.graph.nodes])
 
-    def get_code(self, node):
+    def _get_code(self, node):
         """Construct the general ledger code of an item in the inventory"""
 
         index = nx.get_node_attributes(self.graph, "index")[node]
-        multiplier = 10 ** (self.get_max_depth() - self.get_depth(node))
-        parent = self.get_parent(node)
-        if self.get_depth(node) == 1:
+        multiplier = 10 ** (self._get_max_depth() - self._get_depth(node))
+        parent = self._get_parent(node)
+        if self._get_depth(node) == 1:
             return index * multiplier
         else:
-            return index * multiplier + self.get_code(parent)
+            return index * multiplier + self._get_code(parent)
 
-    def create_codes(self) -> Dict[int, str]:
-        code_item = {self.get_code(nd): nd for nd in self.graph.nodes}
+    def _create_codes(self) -> Dict[int, str]:
+        code_item = {self._get_code(nd): nd for nd in self.graph.nodes}
         return {k: v for k, v in sorted(code_item.items())}
 
-    def add_item_to_graph(self,item: str):
+    def get_code_map(self) ->Dict[int,str]:
+        return {k: v for k, v in sorted(self.code_map.items())}
+
+    def _add_item_to_graph(self, item: str):
         if item in self.graph.nodes:
             raise Exception(f"{item} is already in nodes!")
         else:
             #if item is child of existing category, insert there
-            parents = self.identify_parents(item)
+            parents = self._identify_parents(item)
             if len(parents) == 0:
-                self.insert_root_category(item)
+                self._insert_root_category(item)
             elif len(parents) == 1:
                 parent = parents[0]
+                print("Parent",parent)
                 self.graph.add_node(item)
                 self.graph.add_edge(parent, item)
-                self.code_map[self.get_code(item)] = item
+                #set the index of the new node
+                siblings = self._get_children(parent)
+                print("siblings", siblings)
+                sibling_indices = [ind for nd, ind in nx.get_node_attributes(self.graph,name="index").items() if nd in siblings]
+                max_sibling_index = max(sibling_indices)
+                item_index = max_sibling_index +1
+                nx.set_node_attributes(self.graph, values={item: item_index},name="index")
+                self.code_map[self._get_code(item)] = item
             elif len(parents) > 1:
                 raise NotImplementedError
-    def identify_parents(self,child) -> List[str]:
-        """FIXME instead of querying gpt individually for every node at a depth level
-        we could probably reduce the number of requests (and waiting time) by giving gpt a list of items at that depth
-        and asking whether the item is a member of any of those
-        b"""
+
+    def _identify_parents(self, child) -> List[str]:
+        """Find which category a new item belongs to using NLI"""
         depth_dict = defaultdict(list)
         for n in self.graph.nodes:
-            depth_dict[self.get_depth(n)].append(n)
+            depth_dict[self._get_depth(n)].append(n)
 
-        parents = list()
-        for depth in sorted(range(1, self.get_max_depth()+1), reverse=True):
+        for depth in sorted(range(1, self._get_max_depth() + 1), reverse=True):
+            parents = list()
             items_at_depth = depth_dict[depth]
             for potential_parent in items_at_depth:
-                if isit(child, potential_parent):
+                print(child, potential_parent)
+                if isit(child,potential_parent) > 0.5:
                     parents.append(potential_parent)
             if parents:
                 #we only want the parent at the lowest depth of the tree (furthest from the roots)
@@ -116,6 +138,6 @@ class Inventory:
 
         return parents
 
-    def insert_root_category(self,item):
+    def _insert_root_category(self, item):
         """TODO check first to make sure that we have room for a root category. Then add it."""
         raise NotImplementedError
--- costcurve/nlp.py
+++ costcurve/nlp.py
@@ -1,27 +1,110 @@
-from openai import OpenAI
-
-def get_api_key(path: str = "openai_key.txt"):
+from time import sleep
+from logging import getLogger
+import requests
+from typing import List
+from tqdm import tqdm
+logger = getLogger(__name__)
+def get_huggingface_api_key(path: str = "huggingface_key.txt") -> str:
     with open(path, "r") as f:
         key = f.read()
     return key
 
-def isit(x: str, y: str, client=None) -> bool:
-    """Query GPT3.5 to ask if x is a kind of y."""
-    client = client or OpenAI(api_key=get_api_key())
-    response = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": f"In one word (True or False), is {x} a kind of {y}?",
+def retry(request_fun):
 
-        }
-    ],
-    model="gpt-3.5-turbo",
-)
-    r = response.choices[0].message.content
-    if r.startswith("True"):
-        return True
-    elif r.startswith("False"):
-        return False
-    else:
-        raise Exception(f"Expected True or False from GPT3.5, got {r}")
+    def wrapper(*args, **kwargs):
+        response = request_fun(*args, **kwargs)
+        if "error" in response:
+          if "estimated_time" in response:
+            wait_time = round(response["estimated_time"]) + 1
+            logger.info(f"Waiting {wait_time}s for model to load")
+            sleep(wait_time)
+            response = request_fun(*args, **kwargs)
+          else:
+              raise RuntimeError(f"An error occured: {response}")
+        return response
+
+    return wrapper
+
+def isit(x: str, y: str) -> float:
+    """Query NLI model to ask if x is a kind of y.
+
+    """
+
+    API_URL = "https://api-inference.huggingface.co/models/roberta-large-mnli"
+    headers = {"Authorization": f"Bearer {get_huggingface_api_key()}"}
+    payload = {
+	"inputs": f" x is a {x} Therefore x is a {y}.",
+}
+    response = requests.post(API_URL, headers=headers, json=payload).json()[0]
+    entailment_score = None
+    for label in response:
+        if label["label"] == "ENTAILMENT":
+            entailment_score = label["score"]
+    #FIXME change this to a boolean
+    return entailment_score
+
+@retry
+def get_food_entities(inputs):
+    #TODO we might be able to batch several of these line items into a single request somehow
+    #if rate limiting/cost becomes an issue
+
+    #distillbert seems to behave *very* differently with all caps.
+    line_item = inputs.lower()
+    API_URL = "https://api-inference.huggingface.co/models/chambliss/distilbert-for-food-extraction"
+    headers = {"Authorization": f"Bearer {get_huggingface_api_key()}"}
+    payload = {
+	"inputs": line_item,
+}
+    response = requests.post(API_URL, headers=headers, json=payload).json()
+    return response
+
+
+def parse_line_items(line_items: List[str]) -> List[str]:
+    """
+    Extract the food entities from each line item
+
+    Example:
+
+
+    line_items = ["FRESH SQUID 5-8 R&T (LOLIGO) 1/10# (TUBS) WILD-USA (PRICED BY THE TUB)",
+              "BLACK GROUPER FILLET, SKIN OFF, GULF OG MEXICO, PC RF 40",
+              "FRESH SALMON FILLET, CFW, SKIN ON, FARM, SCOTLAND 10",
+              "FRESH SEA SCALLOPS, U/10 CT. DRY (SEA TRADE) 1 GAL. WILD, USA",
+              "FRESH MUSSELS, BLK. 1/10# FARM, PRINCE EDWARD, ISLAND",
+              "9-11oz. FRESH WHITE TROUT FILLETS B-FLY FARM, COLOMBIA",
+              "Pasteruized Lump Blue Crab Meat (Pelagicus)-Packer 12/1# Wild-Indonesia",
+              "16/20 P&D Tail-on Raw White Shrimp-Packer 5/2# Farm",
+              "6oz. Mahi Portions (IVP)-Packer1/10# Wild-Peru/Vietnam",
+              "SHIPPING AND HANDING"]
+
+    >> parse_line_items(line_items)
+
+    >> ['squid loligo tub',
+     '##er fillet skin g mexico',
+     'salmon fillet cw skin scotland',
+     'sea scallops',
+     'mussels',
+     'white trout fillets b - fly colo',
+     'lump blue crab meat pelagicus',
+     'white shrimp pack',
+     'mahi portions']
+              """
+
+
+    def parse_line_item(line_item: str):
+        food_entities = get_food_entities(line_item)
+        food_entities = [e["word"] for e in food_entities if e["entity_group"] in ["LABEL_0", "LABEL_1"]]
+        return food_entities
+
+    def parse_lst(lst: List[str]):
+        s = lst[0]
+        for it in lst[1:]:
+            if it.startswith("##"):
+                s += it.lstrip("##")
+            else:
+                s += f" {it}"
+        return s
+
+    parsed_items = [parse_line_item(li) for li in tqdm(line_items,"line item")]
+    parsed_lists = [parse_lst(lst) for lst in parsed_items if lst]
+    return parsed_lists
