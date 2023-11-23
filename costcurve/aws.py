02ca064f22e72d8b26749df60710f08dfdaf530b
Daavid Stein <daavid@dendra.io>
2023-11-22 19:17:10 -0500

    gitignore, aws.py

---
+++ costcurve/aws.py
@@ -0,0 +1,57 @@
+import boto3
+from botocore.exceptions import ClientError
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+INVOICE_BUCKET = "costcurve-invoices"
+def get_secret(secret_name):
+
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    return secret
+
+def read_invoices_from_disk(path: str):
+    """Read invoices and if necessary convert them to JPEGs before uploading"""
+    pass
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logger.error(e)
+        return False
+    return True
